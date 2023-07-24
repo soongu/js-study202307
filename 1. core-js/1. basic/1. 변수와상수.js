a2aec4207e9e5e6c228293f5d7b0172958d03156
soongu <naong526@naver.com>
2023-07-24 15:27:46 +0900

    core js - 변수와상수, 데이터타입, 산술연산자 학습

---
+++ 1. core-js/1. basic/1. 변수와상수.js
@@ -0,0 +1,32 @@
+// 변수만들기
+let userName = "뽀로로";
+
+// 재할당
+userName = "잔망루피";
+
+console.log(userName);
+
+// 상수 : 불변의 데이터
+const saleRate = 0.2;
+
+// saleRate = 0.8;
+console.log(saleRate);
+
+// 고전...
+var age = 30;
+
+// 이름 관례 (convention)
+// 개발자들끼리의 약속
+
+// 변수, 함수 이름 : camel case
+// let my_phone_number = '010-1234-1234';
+let myPhoneNumber = "010-1234-1234";
+
+// 들여쓰기 공백은 2칸
+if (true) {
+  if (true) {
+    console.log("hello!");
+  }
+}
+
+
---
+++ 1. core-js/1. basic/3. 산술연산자.js
@@ -0,0 +1,39 @@
+
+// 거듭제곱 연산
+let num = 3;
+console.log(num ** 4);
+
+// 증감 연산자
+let x = 5;
+
+x--;
+console.log(`x: ${x}`);
+
+let y = 33;
+console.log(`y: ${y--}`);
+console.log(`y: ${y}`);
+
+
+let a = 7;
+let b = a++;
+
+console.log(`a: ${a}, b: ${b}`);
+
+let c = 7;
+let d = ++c;
+console.log(`c: ${c}, d: ${d}`);
+
+
+// 대입 연산자
+let z = 9;
+z += 3; // z = z + 3;
+z -= 8; // 4
+z *= 10; // 40
+z /= 8;  // 5
+z **= 2; // 25
+console.log(`z: ${z}`);
+
+let m = 8;
+m++; // 1증가 1감소할 때는 ++, --가 빠름
+// m += 1; // m = m + 1;
+console.log(`m: ${m}`);
