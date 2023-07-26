3cc28a43ba0dceacacbd5de45fd64fa4280e4b4f
soongu <naong526@naver.com>
2023-07-26 10:12:16 +0900

    updown게임 세번째 버전 - 난이도 추가

--- 1. core-js/2. 제어문/9. 점검문제2.js
+++ 1. core-js/2. 제어문/9. 점검문제2.js
@@ -11,17 +11,38 @@
 // 난이도는 상, 중, 하 난이도가 존재하며
 // 난이도별 입력 횟수제한이 다르다.
 
+
+// 난이도 상수
+const HIGH = 1;
+const MIDDLE = 2;
+const LOW = 3;
+
+// 초기 난이도 설정 선택창 띄우기
+let level = +prompt('난이도를 선택하세요!\n# [1. 상(3번의 기회) | 2. 중(6번의 기회) | 3. 하(10번의 기회)]');
+
+// 초기 카운트 수
+let initCount;
+
+
+if (level === HIGH) {
+  initCount = 3;
+} else if (level === MIDDLE) {
+  initCount = 6;
+} else if (level === LOW) {
+  initCount = 10;
+} else {
+  alert('잘못 입력했으니까 난이도 상으로 자동 시작합니다.');
+  initCount = 3;
+}
+
+// 카운트다운 변수
+let countdown = initCount;
+
 // 실제로 정답으로 사용할 랜덤 정수 만들기
 let secret = Math.floor(Math.random() * 100) + 1;
 
 // 입력 최소, 최대값
 let min = 1, max = 100;
-
-// 초기 카운트 수
-let initCount = 5;
-
-// 카운트다운 변수
-let countdown = initCount;
 
 while (true) {
 
