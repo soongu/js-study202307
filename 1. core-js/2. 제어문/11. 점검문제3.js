42046b66307550ec99f45373ae4402e7ecd90d17
soongu <naong526@naver.com>
2023-07-26 10:32:52 +0900

    사칙연산게임 네번째버전 - 난이도 설정 추가

--- 1. core-js/2. 제어문/11. 점검문제3.js
+++ 1. core-js/2. 제어문/11. 점검문제3.js
@@ -1,14 +1,33 @@
-
 let qNum = 1; // 문제 넘버링
 
 // 정답, 오답 횟수
 let correctCount = 0;
 let wrongCount = 0;
 
+let maxNumber;
+
+while (true) {
+  let message = `~~~~~~~~ 난이도를 설정합니다 ~~~~~~~~~~~~
+[1. 상 (1~100) | 2. 중 (1~50) | 3. 하 (1~20) ]`;
+  let level = +prompt(message);
+
+  if (level === 1) {
+    maxNumber = 100;
+  } else if (level === 2) {
+    maxNumber = 50;
+  } else if (level === 3) {
+    maxNumber = 20;
+  } else {
+    alert('난이도를 숫자로 다시 입력하세요!');
+    continue;
+  }
+  break;
+} // end while
+
 while (true) {
   // 랜덤 정수 2개를 생성
-  let firstNumber = Math.floor(Math.random() * 20) + 1;
-  let secondNumber = Math.floor(Math.random() * 20) + 1;
+  let firstNumber = Math.floor(Math.random() * maxNumber) + 1;
+  let secondNumber = Math.floor(Math.random() * maxNumber) + 1;
 
   // 부호를 만들 랜덤 정수 생성
   let markNum = Math.floor(Math.random() * 3);
@@ -16,20 +35,28 @@ while (true) {
   let mark;
   let realAnswer; // 실제 정답
   if (markNum === 0) {
-    mark = '+';
+    mark = "+";
     realAnswer = firstNumber + secondNumber;
   } else if (markNum === 1) {
-    mark = '-';
+    if (firstNumber === secondNumber) continue;
+    else if (firstNumber < secondNumber) {
+      let t = firstNumber;
+      firstNumber = secondNumber;
+      secondNumber = t;
+    }
+    mark = "-";
     realAnswer = firstNumber - secondNumber;
   } else {
-    mark = 'x';
+    mark = "x";
     realAnswer = firstNumber * secondNumber;
   }
-  
-  let userAnswer = +prompt(`Q${qNum++}. ${firstNumber} ${mark} ${secondNumber} = ??`);
+
+  let userAnswer = +prompt(
+    `Q${qNum++}. ${firstNumber} ${mark} ${secondNumber} = ??`
+  );
 
   if (userAnswer === 0) {
-    alert('게임을 종료합니다!');
+    alert("게임을 종료합니다!");
     break;
   }
 
