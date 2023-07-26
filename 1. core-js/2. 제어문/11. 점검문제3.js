5f699398b3e818c53912e5067de51b005fe19c54
soongu <naong526@naver.com>
2023-07-26 10:23:30 +0900

    사칙연산게임 세번째버전 - 뺄셈, 곱셈 기능 추가

--- 1. core-js/2. 제어문/11. 점검문제3.js
+++ 1. core-js/2. 제어문/11. 점검문제3.js
@@ -1,4 +1,3 @@
-// 랜덤 정수 2개를 생성
 
 let qNum = 1; // 문제 넘버링
 
@@ -7,10 +6,27 @@ let correctCount = 0;
 let wrongCount = 0;
 
 while (true) {
+  // 랜덤 정수 2개를 생성
   let firstNumber = Math.floor(Math.random() * 20) + 1;
   let secondNumber = Math.floor(Math.random() * 20) + 1;
+
+  // 부호를 만들 랜덤 정수 생성
+  let markNum = Math.floor(Math.random() * 3);
+
+  let mark;
+  let realAnswer; // 실제 정답
+  if (markNum === 0) {
+    mark = '+';
+    realAnswer = firstNumber + secondNumber;
+  } else if (markNum === 1) {
+    mark = '-';
+    realAnswer = firstNumber - secondNumber;
+  } else {
+    mark = 'x';
+    realAnswer = firstNumber * secondNumber;
+  }
   
-  let userAnswer = +prompt(`Q${qNum++}. ${firstNumber} + ${secondNumber} = ??`);
+  let userAnswer = +prompt(`Q${qNum++}. ${firstNumber} ${mark} ${secondNumber} = ??`);
 
   if (userAnswer === 0) {
     alert('게임을 종료합니다!');
@@ -18,7 +34,7 @@ while (true) {
   }
 
   // 정답 확인
-  if (userAnswer === firstNumber + secondNumber) {
+  if (userAnswer === realAnswer) {
     alert("정답입니다!");
     correctCount++;
   } else {
