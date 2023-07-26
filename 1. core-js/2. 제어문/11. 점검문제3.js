edd5122777f3a4a98eb63a2498cfbff968086ff6
soongu <naong526@naver.com>
2023-07-26 10:16:20 +0900

    사칙연산게임 두번째버전 - 정답, 오답 횟수 카운팅

--- 1. core-js/2. 제어문/11. 점검문제3.js
+++ 1. core-js/2. 제어문/11. 점검문제3.js
@@ -2,6 +2,10 @@
 
 let qNum = 1; // 문제 넘버링
 
+// 정답, 오답 횟수
+let correctCount = 0;
+let wrongCount = 0;
+
 while (true) {
   let firstNumber = Math.floor(Math.random() * 20) + 1;
   let secondNumber = Math.floor(Math.random() * 20) + 1;
@@ -16,7 +20,11 @@ while (true) {
   // 정답 확인
   if (userAnswer === firstNumber + secondNumber) {
     alert("정답입니다!");
+    correctCount++;
   } else {
     alert("틀렸습니다!");
+    wrongCount++;
   }
-}
+} // end while
+
+alert(`정답 횟수: ${correctCount}회, 틀린 횟수: ${wrongCount}회`);
