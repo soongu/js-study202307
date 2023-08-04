5eeb0e68715dcb687401b6b48c231ef7f402900d
soongu <naong526@naver.com>
2023-08-04 13:33:43 +0900

    web-api - 실전예제3: 담기버튼 이벤트 this바인딩 문제 해결
    - ProductList 클래스로 ul생성 분할
    - 이벤트 핸들러 등록시 this가 잘못 바인딩 된 문제 해결

--- 2. web-api/App/Shopping/assets/scripts/app.js
+++ 2. web-api/App/Shopping/assets/scripts/app.js
@@ -15,19 +15,26 @@ const p1 = new Product(
   2000,
   "냠냠박사님 맛있게 밥을 먹어주세요~"
 );
-console.log(p1);
+// console.log(p1);
 const p2 = new Product(
   "쩝쩝이",
   "https://www.animaxtv.co.kr/sites/animaxtv.co.kr/files/ct_character_f_primary_image/nyamnyam.jpg",
   7000,
   "쩝쩝꿀꿀박사님 점심을 추천해주세요~"
 );
-console.log(p2);
+// console.log(p2);
 
 // 한개의 LI태그를 생성하는 컴포넌트 클래스 설계
 class ProductItem {
   constructor(product) {
     this.product = product;
+  }
+
+  // 담기버튼 클릭이벤트 핸들러
+  addToCartHandler() {
+    console.log('장바구니에 상품을 추가함!');
+    // 이 핸들러에서 누른 그 상품의 정보를 알아야 한다.
+    console.log(this.product);
   }
 
   render() {
@@ -44,34 +51,40 @@ class ProductItem {
           </div>
         </div>
       `;
-      return $prod;
+    const $addCartBtn = $prod.querySelector('button');
+    // $addCartBtn.addEventListener('click', this.addToCartHandler.bind(this));
+    $addCartBtn.addEventListener('click', () => this.addToCartHandler());
+    return $prod;
   }
 }
 
-// 상품 목록에 대한 객체
-const productList = {
-  products: [
-    p1,
-    p2,
-    new Product(
-      "햄버거",
-      "https://images.chosun.com/resizer/5jStJ5InVS4u3iHvEzB8y_tGrr8=/616x0/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/HV765ADF7VF4FLG5KISDNFMUJ4.PNG",
-      16900,
-      "꽈뜨로 맥씨멈 스테카 버거~"
-    ),
-    new Product(
-      "애플워치",
-      "https://img.danawa.com/prod_img/500000/535/481/img/15481535_1.jpg?_v=20211215103510",
-      400000,
-      "APPLE watch 2세대 2022"
-    ),
-    new Product(
-      "애플망고",
-      "https://m.thegiboon.com/web/product/big/202104/ea08c22e8939ab1977487abc826b8ab8.jpg",
-      60000,
-      "맛있는 맹고~ 당장 사먹어야지~"
-    ),
-  ],
+// 한 개의 UL을 생성하는 클래스
+class ProductList {
+  constructor() {
+    // 상품들을 모아 놓은 배열
+    this.products = [
+      p1,
+      p2,
+      new Product(
+        "햄버거",
+        "https://images.chosun.com/resizer/5jStJ5InVS4u3iHvEzB8y_tGrr8=/616x0/smart/cloudfront-ap-northeast-1.images.arcpublishing.com/chosun/HV765ADF7VF4FLG5KISDNFMUJ4.PNG",
+        16900,
+        "꽈뜨로 맥씨멈 스테카 버거~"
+      ),
+      new Product(
+        "애플워치",
+        "https://img.danawa.com/prod_img/500000/535/481/img/15481535_1.jpg?_v=20211215103510",
+        400000,
+        "APPLE watch 2세대 2022"
+      ),
+      new Product(
+        "애플망고",
+        "https://m.thegiboon.com/web/product/big/202104/ea08c22e8939ab1977487abc826b8ab8.jpg",
+        60000,
+        "맛있는 맹고~ 당장 사먹어야지~"
+      ),
+    ];
+  } // end constructor
 
   render() {
     // console.log('render!!', this);
@@ -79,14 +92,14 @@ const productList = {
     const $prodList = document.createElement("ul");
     $prodList.classList.add("product-list");
     this.products.forEach((prod) => {
-      //       { product: {title: '', imageUrl: ''}, render() }
       const productItem = new ProductItem(prod);
       // console.log(productItem);
       $prodList.appendChild(productItem.render());
     });
     $app.appendChild($prodList);
-  },
-};
+  }
+}
 
 // 렌더링 명령
+const productList = new ProductList();
 productList.render();
