3afc45ce752a0f199946a4f8be6c0bd75e83e5ea
soongu <naong526@naver.com>
2023-08-09 09:44:52 +0900

    ajax - 종합실습: 일정관리프로그램 - 할일 목록 불러오기

---
+++ 3. ajax/TodoList/assets/scripts/app.js
@@ -0,0 +1,47 @@
+const URL = 'http://localhost:8484/todos';
+
+const $todoList = document.querySelector('.todo-list');
+
+// step1. db.json에 있는 todos를 화면에 렌더링하기
+const fetchTodos = (url, method='GET', payload=null) => {
+  const requestInit = {
+    method: method,
+    headers: { 'Content-Type': 'application/json' }
+  };
+  if (payload) requestInit.body = JSON.stringify(payload);
+
+  return fetch(url, requestInit);
+};
+
+// 화면에 todos를 렌더링하는 함수
+const renderTodos = (todoList) => {
+  // li태그의 템플릿을 가져옴
+  const $liTemplate = document.getElementById('single-todo');
+
+  todoList.forEach(({ id, text, done }) => {
+    // console.log('todo: ', todo);
+    const $newLi = document.importNode($liTemplate.content, true);
+    $newLi.querySelector('li').dataset.id = id; // data-id 속성 부여
+    $newLi.querySelector('.text').textContent = text;
+
+    // 체크박스 가져오기
+    const $checkbox = $newLi.querySelector('.checkbox input[type=checkbox]');
+    // console.dir($checkbox);
+    $checkbox.checked = done;
+
+    done && $checkbox.parentNode.classList.add('checked');
+
+    $todoList.appendChild($newLi);
+  });
+};
+
+// =========== 앱 실행 =========== //
+const init = () => {
+  fetchTodos(URL)
+    .then(res => res.json())
+    .then(todos => {
+      renderTodos(todos);
+    });
+};
+
+init();
